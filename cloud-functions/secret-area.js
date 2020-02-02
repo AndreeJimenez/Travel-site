ac490b23cbdd97127344e26bb1229908bda010e1
Andree <andree_jimenez@outlook.com>
2020-02-02 08:35:47 -0600

    Changed password variable.

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -15,7 +15,7 @@ exports.handler = function (event, context, callback) {
     if (body.password == "javascript") {
         callback(null, {
             statusCode: 200,
-            body: "secretContent"
+            body: secretContent
         })
     } else {
         callback(null, {
