5bb594364506fdcd4c5b857f690dec19a1a213b5
Andree <andree_jimenez@outlook.com>
2020-02-02 08:29:31 -0600

    Updated cloud function to check password.

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,25 @@
-exports.handler = function(event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: "Welcome to the super secret area"
-    })
+exports.handler = function (event, context, callback) {
+    const secretContent = `
+    <h3>Welcome To The Secret Area</h3>
+    <p>Here is the <strong>Secret</strong> area</p>
+    `
+
+    let body;
+
+    if (event.body) {
+        body = JSON.parse(event.body)
+    } else {
+        body = {}
+    }
+
+    if (body.password == "javascript") {
+        callback(null, {
+            statusCode: 200,
+            body: "secretContent"
+        })
+    } else {
+        callback(null, {
+            statusCode: 401,
+        })
+    }
 }
