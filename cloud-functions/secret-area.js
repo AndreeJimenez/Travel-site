3b2f140d6d946674932b1a80663b47b530d6ced5
Andree <andree_jimenez@outlook.com>
2020-02-02 18:12:32 -0600

    Test Cloud Funtion from Localhost.

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,4 +1,17 @@
 exports.handler = function (event, context, callback) {
+    const headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type"
+    }
+
+    if (event.httpMethod !== "POST") {
+        return callback(null, {
+            statusCode: 200,
+            headers,
+            body: "This was not a POST request"
+        })
+    }
+
     const secretContent = `
     <h3>Welcome To The Secret Area</h3>
     <p>Here is the <strong>Secret</strong> area</p>
@@ -15,11 +28,13 @@ exports.handler = function (event, context, callback) {
     if (body.password == "javascript") {
         callback(null, {
             statusCode: 200,
+            headers,
             body: secretContent
         })
     } else {
         callback(null, {
             statusCode: 401,
+            headers
         })
     }
 }
